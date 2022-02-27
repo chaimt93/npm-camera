9262a48821ee8da34faca0cf028e6d406a9ddb83
chaim <chaimt93@gmail.com>
2022-02-28 00:23:40 +0200

    working on mobile + custom style enabled

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+/* eslint-disable */
 let video = null;
+let stream = null;
 let videoSize = {};
 let isMobile;
 
 async function init() {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({video: true});
+        stream = await navigator.mediaDevices.getUserMedia({video: true});
         let {width, height} = stream.getTracks()[0].getSettings();
-        isMobile= width>height
+        isMobile = width > height
         videoSize.width = width
         videoSize.height = height
         video = createVideo();
@@ -17,7 +19,27 @@ async function init() {
         console.error(e)
     }
 }
+function snap() {
+    const canvas = document.createElement("canvas");
+    canvas.width = videoSize.width;
+    canvas.height = videoSize.height;
+    const context = canvas.getContext("2d");
+    const drawImageParameters = generateDrawImageParameters(canvas, canvas.width, canvas.height);
+    context.scale(-1, 1);
+    context.drawImage(video, ...drawImageParameters);
+    const imageBase64 = canvas.toDataURL("image/png");
+    return imageBase64.slice()
+}
+function stop(){
+    stream.getTracks().forEach(track => track.stop())
+}
+function setStyle(style){
+    if (!style)return;
+    Object.keys(style).forEach(key=>{
+        video.style[key] = style[key]
+    })
 
+}
 function createVideo() {
     const video = document.createElement("video");
     video.id = "selfieVideo";
@@ -26,36 +48,13 @@ function createVideo() {
     video.autoplay = true;
     return video;
 }
-
-function snap() {
-    const canvas = document.createElement("canvas");
-    canvas.width = videoSize.width;
-    canvas.height = videoSize.height;
-    const context = canvas.getContext("2d");
-    const drawImageParameters = this.generateDrawImageParameters(canvas, canvas.width, canvas.height);
-    console.log(drawImageParameters)
-    context.scale(-1, 1);
-    context.drawImage(video, ...drawImageParameters);
-    const imageBase64 = canvas.toDataURL("image/png");
-    return imageBase64
-}
-
 function generateDrawImageParameters(canvas, videoWidth, videoHeight) {
     if (isMobile) return [0, 0, videoWidth, videoHeight, 0, 0, -videoWidth, videoHeight];
-
-    // if (isMobile) return [0, 0, canvas.width, canvas.height, 0, 0, canvas.width * -1, canvas.height];
     return [0, 0, videoWidth, videoHeight, 0, 0, -videoWidth, videoHeight]
 }
 
 
-function createSelfieVideo() {
-    const video = document.createElement("video");
-    video.id = "selfieVideo";
-    video.style.width = "300px";
-    video.style.height = "300px";
-    video.style.clipPath = "circle(37%)";
-    video.style.webkitTransform = "scaleX(-1);";
-    video.style.transform = "scaleX(-1)";
-    video.autoplay = true;
-    return video;
+export default {
+    init, snap, stop ,setStyle
 }
+/* eslint-disable */
